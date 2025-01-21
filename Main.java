98d57c6aa3078aa0da1d576fadf315d4b0edda7e
felipekm <felipe.kautzmann@gmail.com>
2025-01-21 11:35:24 -0300

    feat:Format License Key Code

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Give a string S - alphanumerical with dashes
+
+
+
+        // Input: S = “5F3Z-2e-9-w”,
+        // K = 4
+        // Output: “5F3Z-2E9W”
+
+    }
+}
